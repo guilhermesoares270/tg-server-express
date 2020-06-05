dbf7c92d44828a97741b81c352331be317a33ec3
Guilherme Leite <guilherme.soares270@gmail.com>
2020-06-05 17:44:27 -0300

    init contract erro catching

--- app/Services/InitContracts.js
+++ app/Services/InitContracts.js
@@ -4,19 +4,23 @@ const Wallet = require('../Services/EnterpriseWallet');
 const connection = require('../connection');
 
 const findContracts = async () => {
-    const { abi } = ContractHelper.readContract().contracts['generic_contract.sol']['Docs'];
+    try {
+        const { abi } = ContractHelper.readContract().contracts['generic_contract.sol']['Docs'];
 
-    const query = 'SELECT razao_social as rs, cnpj as cnpj, contract_address as ca FROM enterprises LIMIT 500';
-    const enterprises = await connection.query(query, {
-        type: connection.QueryTypes.SELECT
-    });
+        const query = 'SELECT razao_social as rs, cnpj as cnpj, contract_address as ca FROM enterprises LIMIT 500';
+        const enterprises = await connection.query(query, {
+            type: connection.QueryTypes.SELECT
+        });
 
-    enterprises.forEach(x => {
-        const enterpriseContract = new Wallet.eth.Contract(abi, x.ca);
-        ContractInstance.addContract(enterpriseContract, x.rs, x.cnpj);
-    });
+        enterprises.forEach(x => {
+            const enterpriseContract = new Wallet.eth.Contract(abi, x.ca);
+            ContractInstance.addContract(enterpriseContract, x.rs, x.cnpj);
+        });
 
-    console.log(`lo: ${ContractInstance.contracts.length}`);
+        console.log(`lo: ${ContractInstance.contracts.length}`);
+    } catch (err) {
+        console.log(`Error loading contracts: ${err}`);
+    }
 };
 
 module.exports = findContracts;
--- config/database.js
+++ config/database.js
@@ -3,4 +3,10 @@ require('dotenv').config();
 module.exports = {
   url: process.env.DATABASE_URL,
   dialect: 'postgres',
+  // ssl: true,
+  // dialectOptions: {
+  //   "ssl": {
+  //     "require": true
+  //   }
+  // }
 };
